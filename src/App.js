814e3c4887643f0370cdfadc9d750695bfd75282
zenith <zenithyap88@gmail.com>
2025-02-04 12:12:08 +0800

    Create navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div>Hello world</div>
+    <div>
+      <Navbar></Navbar>
+      <div>Hello world</div>
+    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import "../styles.scss";
+
+function Navbar() {
+    return (
+        <div id="navbar">
+            <div>Zenith</div>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Hobbies</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
