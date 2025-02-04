98add2809a0689f8dbb47adb24c58c465a35ff6c
zenith <zenithyap88@gmail.com>
2025-02-04 14:40:36 +0800

    Add landing page with stars

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
-      <div>Hello world</div>
+      <LandingPage></LandingPage>
     </div>
   );
 }
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import "../../styles.scss";
+import Stars from "./Stars";
+
+function LandingPage() {
+    return (
+        <section id="landing-page">
+            <Stars></Stars>
+            <h3>Hello, this is</h3>
+            <h1>Zenith Yap</h1>
+            <h3>Aspiring web developer</h3>
+        </section>
+    );
+}
+
+export default LandingPage;
