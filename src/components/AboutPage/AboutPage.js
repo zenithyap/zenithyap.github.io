24692b6c8cd3adfca3f09b47968dc8ca668975e5
zenith <zenithyap88@gmail.com>
2025-02-06 14:01:19 +0800

    Style AboutPage

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,11 +1,14 @@
 import "../../styles.scss";
+import profilePic from "./profilePic.png";
 
 function AboutPage() {
     return (
         <section id="about-page">
             <h1>About</h1>
-            <img src="" alt="My Profile Picture" />
-            <p>Introduction</p>
+            <div className="introduction-container">
+                <img src={profilePic} alt="My Profile" />
+                <p>Hey, welcome to my website! I am a final-year Computer Science undergraduate at the National University of Singapore. I enjoy building visually engaging and dynamic web experiences using modern web technologies. Outside of coding, I am a reader, gamer, and a lifelong learner.</p>
+            </div>
         </section>
     );
 }
