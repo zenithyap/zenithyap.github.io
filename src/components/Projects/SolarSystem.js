25c71ff0674da195cde506ef4b120448a191cef8
zenith <zenithyap88@gmail.com>
2025-02-17 11:01:17 +0800

    Add mobile support for planets layout

--- src/components/Projects/SolarSystem.js
+++ src/components/Projects/SolarSystem.js
@@ -10,12 +10,28 @@ function SolarSystem() {
     });
 
     function handlePlanetClick(planet) {
-        setPlanetDesc(prev => ({
-            ...prev,
-            [planet]: !prev[planet]
-        }));
+        const animatedBox = document.querySelector(`#${planet} + .animated-box`);
+        console.log(animatedBox);
+        setPlanetDesc(prev => {
+            const updatedPlanetDesc = {
+                ...prev,
+                [planet]: !prev[planet]
+            }
+
+            if (updatedPlanetDesc[planet] === true) {
+                animatedBox.style.width = "fit-content";
+                animatedBox.style.height = "auto";
+            } else {
+                setTimeout(() => {  
+                    animatedBox.style.width = 0;
+                    animatedBox.style.height = 0;
+                }, 1250);
+            }
+
+            return updatedPlanetDesc;
+        });
     };
-    
+
     const pixDesc = "A command-line task manager done as a school project. It can mark/unmark, add/delete, and find tasks by keyword.";
     const myLibDesc = "Bookmark application done as a school project in a team of five. My role was designing and implementing the front end design. The app is able to support books that currently being read, finished books as well as books to read."
 
