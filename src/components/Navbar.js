0f2cc15da163bdcc8c028ac42bfd6d8bdcaa0968
zenith <zenithyap88@gmail.com>
2025-02-18 16:38:55 +0800

    Add collapsible navbar on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
 import "../styles.scss";
+import { useState } from "react";
 
 function Navbar() {
+    const [showNavbar, setShowNavbar] = useState(false);
+
+    function handleHamburgerClick() {
+        setShowNavbar(prev => !prev);
+    };
+
     return (
         <div id="navbar">
             <div>Zenith</div>
-            <ul>
-                <li><a href="#landing-page">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="">Hobbies</a></li>
-            </ul>
+            <div id="menu">
+                <div id="hamburger" onClick={handleHamburgerClick}>&#9776;</div>
+                <ul className={ showNavbar ? "display" : "" }>
+                    <li><a href="#landing-page">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="">Hobbies</a></li>
+                </ul>
+            </div>
         </div>
     );
 }
