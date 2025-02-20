a49289dab7b38ae1e97528c22db3c76d55c0286f
zenith <zenithyap88@gmail.com>
2025-02-20 09:57:45 +0800

    Change saturn image

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,12 +1,12 @@
 import "./SkillsStyles.scss";
-import planet from "./planet.png";
+import saturn from "./saturn.png";
 
 function Skills() {
     return (
         <div id="skills">
             <h1>Skills</h1>
             <div className="scene">
-                <img src={planet} alt="Planet" />
+                <img src={saturn} alt="saturn" />
                 <div className="banner">
                     <div className="panel"></div>
                     <div className="panel"></div>
