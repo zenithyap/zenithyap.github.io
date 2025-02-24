42eeb8a7d3daed7e398aba3f86ef7093e0567040
zenith <zenithyap88@gmail.com>
2025-02-24 11:08:24 +0800

    Remove hamburger for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,14 @@
 import "../styles.scss";
-import { useState } from "react";
+
 
 function Navbar() {
-    const [showNavbar, setShowNavbar] = useState(false);
-
-    function handleHamburgerClick() {
-        setShowNavbar(prev => !prev);
-    };
-
     return (
         <div id="navbar">
-            <div id="menu">
-                <div id="hamburger" onClick={handleHamburgerClick}>&#9776;</div>
-                <ul className={ showNavbar ? "display" : "" }>
-                    <li><a href="#landing-page">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                </ul>
-            </div>
+            <ul>
+                <li><a href="#landing-page">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+            </ul>
         </div>
     );
 }
